f81a84a6ae0825000c46d2b38832691e72bb918e
איתי <@DESKTOP-5CAMIR6>
2025-01-28 13:53:38 +0200

    a

--- server/api/routes/manager.js
+++ server/api/routes/manager.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { addBranch,addRegularEmployee ,getBranchesByCustomer,
         addCustomer,getAllCustomers,
-        getCleaningsByBranch , addCleaningForEmployee} = require('../controller/manager');
+        getCleaningsByBranch , addCleaningForEmployee } = require('../controller/manager');
 
 const { isManager,isAuthenticated } = require('../middleware/authMiddleware');
 
