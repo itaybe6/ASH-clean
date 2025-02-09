568760563d034d75ab51a9fde83bf0d78bc24111
איתי <@DESKTOP-5CAMIR6>
2025-02-09 10:44:16 +0200

    Force new deployment to Heroku

--- server/server.js
+++ server/server.js
@@ -38,5 +38,5 @@ setInterval(deleteOldCleanings, 24 * 60 * 60 * 1000);
 // Start server
 const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`); //test
 });
