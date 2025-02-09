0fec1db38300462b16f4a0eff080471cbd3154f0
איתי <@DESKTOP-5CAMIR6>
2025-02-09 17:05:41 +0200

    Fix Procfile and dependencies

--- server/server.js
+++ server/server.js
@@ -2,27 +2,45 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path'); // כדי להגיש את צד הלקוח
+
+// ייבוא ראוטים
 const authRoutes = require('./api/routes/auth');
-const custumerRoutes = require('./api/routes/costumer');
+const costumerRoutes = require('./api/routes/costumer');
 const workerRoutes = require('./api/routes/worker');
 const managerRoutes = require('./api/routes/manager');
+
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
 
+// התחברות למסד הנתונים MongoDB
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// שימוש בנתיבים של ה-API
 app.use('/auth', authRoutes);
 app.use('/manager', managerRoutes);
-app.use('/customer', custumerRoutes);
+app.use('/costumer', costumerRoutes);
 app.use('/worker', workerRoutes);
-const deleteOldCleanings = require('./api/cleaningJob');
-setInterval(deleteOldCleanings, 24 * 60 * 60 * 1000); 
-const PORT = process.env.PORT ||5000;
-app.listen(PORT, () => {});
+
+const clientBuildPath = path.join(__dirname, '../dist'); 
+app.use(express.static(clientBuildPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
+
+// הגדרת פורט
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
