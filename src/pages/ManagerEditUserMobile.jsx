4c2c9aebbc71ce69913795a35f324133e24ef1a2
איתי <@DESKTOP-5CAMIR6>
2025-02-12 09:41:50 +0200

    Prodaction

--- src/pages/ManagerEditUserMobile.jsx
+++ src/pages/ManagerEditUserMobile.jsx
@@ -19,7 +19,8 @@ const ManagerEditUserMobile = () => {
   const handlePhoneNumberChange = (e) => setPhoneNumber(e.target.value);
   const handleCityChange = (e) => setCity(e.target.value);
   const navigate = useNavigate();
-  const { id, type } = useParams(); // קבלת ה-ID מה-URL
+  const { id, type } = useParams();
+
   const handleUpdateDetails = async (e) => {
     e.preventDefault();
     const updatedData = {
@@ -41,8 +42,6 @@ const ManagerEditUserMobile = () => {
       console.error('Error updating user:', error);
       alert('שגיאה בעדכון הנתונים');
     }
-
-
   };
   const menu = () => {
     setDisplayMenu(!displayMenu)
@@ -51,6 +50,39 @@ const ManagerEditUserMobile = () => {
   const closeMenu = () => {
     setDisplayMenu(false);
   };
+
+
+  const deleteCustomer = async () => {
+    try {
+      const response = await axios.delete(`${apiUrl}/manager/delete-customer/${id}`);
+      console.log('✅ מחיקה הצליחה:', response.data.message);
+      navigate(`/manager-display-users`)
+    } catch (error) {
+      console.error('❌ שגיאה במחיקת הלקוח:', error.response?.data?.error || error.message);
+      navigate(`/manager-display-users`)
+    }
+  };
+  const deleteEmployee = async () => {
+    try {
+      const response = await axios.delete(`${apiUrl}/manager/delete-employee/${id}`);
+      console.log('✅ מחיקת עובד הצליחה:', response.data.message);
+      navigate(`/manager-display-users`)
+    } catch (error) {
+      console.error('❌ שגיאה במחיקת עובד:', error.response?.data?.error || error.message);
+      navigate(`/manager-display-users`)
+    }
+  };
+  const deleteUser = async () => {
+    const isConfirmed = window.confirm('האם אתה בטוח שברצונך לבצע פעולה זו ?');
+    if (isConfirmed) {
+      if (type == 'לקוח') {
+        deleteCustomer();
+      }
+      else {
+        deleteEmployee();
+      }
+    }
+  }
   return (
     <div className="manager-edit-profile-mobil">
       {displayMenu ? <MobileMenuManager isOpen={displayMenu} closeMenu={closeMenu} /> : null}
@@ -62,43 +94,18 @@ const ManagerEditUserMobile = () => {
           <div className="component-child1" />
           <b className="b50">עדכן פרטים</b>
         </button>
-        <input
-          className="group-child82"
-          placeholder="שם מלא"
-          type="text"
-          value={fullName}
-          onChange={handleFullNameChange}
-          required
-        />
-        <input
-          className="group-child83"
-          placeholder="בחר כאן סיסמא חדשה"
-          type="password"
-          value={newPassword}
-          onChange={handleNewPasswordChange}
-          required
-        />
-       
-        <input
-          className="group-child85"
-          placeholder="מספר פלאפון"
-          type="tel"
-          value={phoneNumber}
-          onChange={handlePhoneNumberChange}
-          required
-        />
-        <input
-          className="group-child86"
-          placeholder="עיר"
-          type="text"
-          value={city}
-          onChange={handleCityChange}
-          required
-        />
+        <input className="group-child82" placeholder="שם מלא" type="text" value={fullName} onChange={handleFullNameChange} required />
+        <input className="group-child83" placeholder="בחר כאן סיסמא חדשה" type="password" value={newPassword} onChange={handleNewPasswordChange} required />
+        <input className="group-child85" placeholder="מספר פלאפון" type="tel" value={phoneNumber} onChange={handlePhoneNumberChange} required />
+        <input className="group-child86" placeholder="עיר" type="text" value={city} onChange={handleCityChange} required />
         <div className="div108">הגדר סיסמא חדשה</div>
       </div>
       <button className="vector-wrapper39" onClick={menu}>
         <img className="vector-icon48" alt="" src="/vector10.svg" />
+      </button>
+
+      <button className="vector-wrapper76" onClick={deleteUser}>
+        <img className="vector-icon46" alt="" src="/vector17.svg" />
       </button>
       <img className="icon24" alt="" src="/-02-11@2x.png" />
     </div>
